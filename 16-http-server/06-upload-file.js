58e3cf201219bd951f0ad98849b11a3a06a472b5
huangfuyuru <864134710@qq.com>
2019-10-15 16:01:14 +0800

    a

--- 16-http-server/06-upload-file.js
+++ 16-http-server/06-upload-file.js
@@ -12,21 +12,23 @@ http.createServer((req, res) =>{
   
   req.setEncoding('binary');//设置编码格式
   req.on('data',(data)=>{
-    f1 += data
-  })
-  req.on('end',()=>{
-  //get file name
-  
-  var filename1 = qs.parse(f1.split('\r\n')[1].split(';')[2].trim()).filename;
-  filename1 = filename1.slice(1,filename1.length-1);
+    f1 += data;
+  });
 
-  //get file data
-  var filedata1 = f1.split('\r\n')[4];
-  log(new Buffer(f1.split('\r\n')[4]).toString('utf8'))
-  })
+  req.on('end',()=>{
+    //get file name
+    var filename1 = qs.parse(f1.split('\r\n')[1].split(';')[2].trim()).filename;
+    filename1 = filename1.slice(1,filename1.length-1);
+  
+    //get file data
+    var filedata1 = f1.split('\r\n')[4];
+    //log(new Buffer(f1.split('\r\n')[4]).toString('utf8'));
+
+    //step2:save file
+    fs.writeFileSync(filename1,filedata1,{'enconding':'binary'})
+  });
 
   //step2:save file
-  fs.writeSync(filename1,filedata1,{'enconding':'binary'})
   req.pipe(process.stdout);
   res.end('OK!')
 }).listen(8080)
