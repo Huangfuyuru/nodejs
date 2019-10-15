52dcc8cd36e9675e3241b5db136dd45d866a5d1e
Huangfuyuru <https://github.com/Huangfuyuru/JS-advanced-lessons.git>
2019-10-15 20:23:06 +0800

    a

---
+++ 16-http-server/todo.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+  var todo = document.getElementById('todo');
+  var btnOk = document.getElementById('okButton');
+  var out = document.getElementById('output');
+  var items = [];
+  getItems();
+  todo.onclick=function(){
+    if(todo.value === '') return;
+    fetch('http://192.168.102.144:8080',{method:'POST',body:todo.value})
+    items.push(todo.value);
+    showData();
+}
+  function getItems(){
+    fetch('http://192.168.102.144:8080/').then((res)=>{
+      res.text().then((txt)=>{
+        items = JSON.parse(txt);
+        showData();
+      })
+    })
+}
+  function showData(){
+    out.innerHTML = '';
+    var ul = document.createElement('ul');
+    ul.innerHTML=items.map((item)=>{
+      return '<li>'+item+'</li>';
+    }).join('\n');
+    out.appendChild(ul);
+  }
+
+}
