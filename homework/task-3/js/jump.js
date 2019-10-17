c5e7ae0781cf18733090de481d11b468dbed798f
huangfuyuru <864134710@qq.com>
2019-10-17 15:01:28 +0800

    a

--- homework/task-3/js/jump.js
+++ homework/task-3/js/jump.js
@@ -1,7 +1,9 @@
 window.onload=function(){
-    function fetchMsg(){
+    var dian = document.getElementById('dian');
+
+    dian.onclick=function(){
         console.log('jump');
-        fetch('http://192.168.102.144:8080/detail?chapterId=4').then((res)=>{
+        fetch('http://192.168.102.144:8080/login/').then((res)=>{
             console.log('fetch');
             return res;
         })
--- homework/task-3/web-server.js
+++ homework/task-3/web-server.js
@@ -2,14 +2,17 @@
 const http = require('http'),
       log = console.log,
       fs = require('fs'),
-      url = require('url');
+      url = require('url'),
+      qs = require('querystring');
 
 http.createServer((req,res)=>{
   var path = url.parse(req.url);
   var pathQuery = path.query;
+  var itemQuery = qs.parse(pathQuery);
   var file = __dirname;
   var fileitem = [];
-  if(pathQuery != 'undefined'){
+  
+  if(pathQuery == null){
   switch(req.url){
     case '/list/':
       file += '/chapterList.html';
@@ -32,16 +35,15 @@ http.createServer((req,res)=>{
       break;
   }
   }
-  log(pathQuery);
-  log(file);
-  
+  if(itemQuery.chapterId == '4'){
+    file += '/chapter.html'
+  }
   fs.readFile(file,(err,data)=>{
     if(err){
       log(err.message);
       res.statusCode = 404;
       res.end(err.message);
     }else{
-      log(file);
       res.writeHead(200,{
         'Content-Type':'text/html'
       })
