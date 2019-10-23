55e6ac62d0f81a2ec670bf3d920b3ed9f6d13a10
huangfuyuru <864134710@qq.com>
2019-10-23 20:07:12 +0800

    a

--- 17-http-advanced/02-cookie-opt.js
+++ 17-http-advanced/02-cookie-opt.js
@@ -12,10 +12,11 @@ http.createServer((req,res)=>{
   if(typeof req.headers.cookie !== 'undefined'){
     var data = req.headers.cookie.split(';');
     log('data',data);
+    res.end('jiexi')
   }
 
-  res.setHeader('Set-cookie',['name=wangding;max-age=1000','age=41']);
-  
+  res.setHeader('Set-cookie',['name=wangding;max-age=1000','age=41']);  
   //max-age:1000  给这个cookie加上了1000s的时间，是个长期
+  //设置了两个字段，当他们存在客户端中用;分割
   res.end('OK');
 }).listen(8080);
--- 17-http-advanced/04-cookie-auth.js
+++ 17-http-advanced/04-cookie-auth.js
@@ -1,54 +1,93 @@
 #!/usr/bin/node
 const http = require('http'),
       log = console.log,
-      fs = require('fs'),
       qs = require('querystring');
 
-var islogin = false;
-var login = fs.readFileSync('./login.html','utf8');
-var home = fs.readFileSync('./home.html','utf8');
+var isLogin;
 http.createServer((req,res)=>{
-  log(`${req.method} ${req.url} ${req.httpVersion}`);
-  log(req.headers);
-  log('');
+  var data = '';
   
+  if(typeof req.headers.cookie === 'undefined'){
+    isLogin = false;
+  }else{
+    var pair = req.headers.cookie.split('=');
+    isLogin = (pair[1] === 'true');
+  }
 
-  if(req.url === '/login' && req.method === 'POST'){
-    var user = '';
-    req.on('data',(data)=>{
-      user += data
-    })
+  console.log(isLogin);
 
+  if(req.method === 'POST' && req.url === '/login'){
+    req.on('data',(chunk)=>{
+      data += chunk;
+    });
     req.on('end',()=>{
-      var usr = qs.parse(user);
-      if(usr.username === 'wangding' && usr.password === '123'){
-        islogin = true;
-        res.setHeader('Set-Cookie',`login=${islogin}`)
-        res.end(home);
+      var account = qs.parse(data);
+      if(account.user === 'wangding' && account.password === '123'){
+        isLogin = 'true';
+        showHome(res)
       }else{
-        res.end(login);
+        showLogin(res);
       }
     })
-  } 
-
-  if(req.url === '/logout' && req.method === 'GET'){
-    islogin = false;
-    res.setHeader('Set-Cookie',`login=${islogin}`);
-    res.end(login);
-    return;
   }
-  
 
-  
-  if(typeof req.headers.cookie !== 'undefined'){
-    var data = req.headers.cookie.split('=');
-    islogin = data[1];
-    if(islogin === 'true'){
-      res.end(home);
+  if(req.method === 'GET'){
+    if(isLogin){
+      if(req.url === '/logout'){
+        isLogin = false;
+        res.setHeader('Set-cookie', `login=${isLogin}; max-age=600`);
+        showLogin(res);
+      }else{
+        showHome(res);
+      }
     }else{
-      res.end(login)
+      showLogin(res);
     }
   }
+  
 
-  res.end(login);
 }).listen(8080);
+
+function showHome(res){
+   var html='<!DOCTYPE html>'
+            + '<html>'
+            + '  <head>'
+            + '    <meta charset="UTF-8">'
+            + '    <title>home</title>'
+            + '  </head>'
+          + '    <body>'
+          + '       <h1>This is home page, you are login!</h1>'
+          + '       <a href="/logout">logout</a>'
+          + '    </body>'
+            + '</html>';
+   
+   res.writeHead(200,{
+     'Content-Type':'text/html',
+     'Content-Length':Buffer.byteLength(html),
+     'Set-cookie':`login=${isLogin};max-age=600`
+   })
+   res.end(html);
+
+}
+function showLogin(res){
+  var html = '<!DOCTYPE html>'
+            + '<html>'
+            + '  <head>'
+            + '    <meta charset="UTF-8">'
+            + '    <title>登录</title>'
+            + '  </head>'
+            + '  <body>'
+            + '    <form method="post" action="/login">'
+            + '       <p>用户名：<input type="text" name="user"></p>'
+            + '       <p>密　码：<input type="password" name="password"></p>'
+            + '       <p><input type="submit" value="登录"></p>'
+            + '    </form>'
+            + '  </body>'
+            + '</html>';
+  res.writeHead(200,{
+    'Content-Type':'text/html',
+    'Content-Length':Buffer.byteLength(html)
+  })
+  res.end(html)
+}
+
